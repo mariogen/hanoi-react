6acdac20a59d61253aa6db0f1fc35ab5e05288d0
Mario Gomes Neves Neto <mario.neves@petrobras.com.br>
2020-06-08 22:10:23 -0300

    Updates

---
+++ precache-manifest.2c1d10b952b23cddebbf1029d282f5ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "486fba850afaa7be9a7ef04d477ccf22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0605fc6d819cf7f0ed70",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fb0e8a1d04f84504e3bc",
+    "url": "/static/js/2.00a28ca5.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/static/js/2.00a28ca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0605fc6d819cf7f0ed70",
+    "url": "/static/js/main.646800c8.chunk.js"
+  },
+  {
+    "revision": "26a2aa1d6790800d7daf",
+    "url": "/static/js/runtime-main.41e1990a.js"
+  }
+]);
