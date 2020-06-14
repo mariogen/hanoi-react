13a81fd86041606c6499f129652a98718bfda659
Mario Gomes Neves Neto <mario.neves@petrobras.com.br>
2020-06-14 19:35:32 -0300

    Updates

--- precache-manifest.04b5396cb722ef719199a7291bf01194.js
+++ precache-manifest.04b5396cb722ef719199a7291bf01194.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a07c909c483537e683672ce2070008b9",
+    "revision": "61f5c4d899500b041620b2abfaf11cb7",
     "url": "/index.html"
   },
   {
-    "revision": "3f140af75232340781fa",
+    "revision": "22b1f947d90ffe5267df",
     "url": "/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "fb0e8a1d04f84504e3bc",
-    "url": "/static/js/2.00a28ca5.chunk.js"
+    "revision": "23cb9c217a4a894b6180",
+    "url": "/static/js/2.7eabc97f.chunk.js"
   },
   {
     "revision": "68ba00d5eb083746d913686923a3d904",
-    "url": "/static/js/2.00a28ca5.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.7eabc97f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3f140af75232340781fa",
-    "url": "/static/js/main.19f4e387.chunk.js"
+    "revision": "22b1f947d90ffe5267df",
+    "url": "/static/js/main.3ba9b0dd.chunk.js"
   },
   {
     "revision": "26a2aa1d6790800d7daf",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.49134055ed3911d90ffc48a7d0dbd434.js"
+  "/precache-manifest.04b5396cb722ef719199a7291bf01194.js"
 );
 
 self.addEventListener('message', (event) => {
